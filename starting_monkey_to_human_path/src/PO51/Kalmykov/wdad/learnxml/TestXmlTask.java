c0d8c354451d54fed22e294be90950ba615af7f2
vkalmykov <pitoshkaa@gmail.com>
2017-10-04 09:59:32 +0400

    1-3 tasks complete

---
+++ starting_monkey_to_human_path/src/PO51/Kalmykov/wdad/learnxml/TestXmlTask.java
@@ -0,0 +1,22 @@
+package PO51.Kalmykov.wdad.learnxml;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class TestXmlTask {
+    public static void main (String[] str) throws Exception {
+        XmlTask xml = new XmlTask("src\\PO51\\Kalmykov\\wdad\\learnxml\\test3.xml");
+        String oldSecondName = "Ivanov";
+        String oldFirstName = "Ivan";
+        String newSecondName = "Petrov";
+        String newFirstName = "Petr";
+        Calendar calendar = new GregorianCalendar(2001, 10, 16);
+        System.out.println(xml.earningsTotal(newSecondName, calendar));
+        xml.changeOfficiantName(oldFirstName, oldSecondName, newFirstName, newSecondName);
+        calendar.clear();
+        calendar.set(2001, 10, 16);
+        xml.removeDay(calendar);
+        
+    }
+
+}
--- starting_monkey_to_human_path/src/PO51/Kalmykov/wdad/learnxml/XmlTask.java
+++ starting_monkey_to_human_path/src/PO51/Kalmykov/wdad/learnxml/XmlTask.java
@@ -4,61 +4,122 @@ import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.File;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 
 public class XmlTask {
     private final File file;
-    private Document doc;
+    private final Document doc;
+    private final String path;
     
     public XmlTask(String path) throws Exception {
-        file = new File(path);
+        this.path = path;
+        file = new File(this.path);
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         doc = db.parse(file);
+        doc.normalize();
     }
     
-    public int earningsTotal(String officiantSecondName, Calendar calendar) {
+    public int earningsTotal(String officiantSecondName, Calendar calendar) throws TransformerException {
         int total=0, day, year, month;
-        Calendar thisCalendar = null;
-        NodeList nodeList = doc.getElementsByTagName("date");
-        NodeList orderList;
+        Calendar thisCalendar = new GregorianCalendar();
+        NodeList dateList = doc.getElementsByTagName("date");                   //массив date'ов
+        NodeList orderList;                                                     // массив order'ов
+        NodeList bufList;
         String secondName;
         Element element;
-        for (int i=0; i<nodeList.getLength(); i++)
+        Element orderElement;                                                   //переменная будет содержать item из orderList
+        for (int i=0; i<dateList.getLength(); i++)
         {
-            element = (Element) nodeList.item(i);
+            element = (Element) dateList.item(i);
             year = Integer.parseInt(element.getAttribute("year"));
             month = Integer.parseInt(element.getAttribute("month"));
             day = Integer.parseInt(element.getAttribute("day"));
+            thisCalendar.clear();
             thisCalendar.set(year, month, day);
             if (calendar.equals(thisCalendar))
             {
                 orderList = element.getElementsByTagName("order");
                 for (int k=0; k<orderList.getLength(); k++)
                 {
-                    element = (Element) orderList.item(k);
+                    orderElement = (Element) orderList.item(k);
+                    element = (Element) element.getElementsByTagName("officiant").item(0);
+                    secondName = element.getAttribute("secondname");
+                    if (officiantSecondName.equals(secondName))
+                    {
+                        
+                        bufList = orderElement.getElementsByTagName("item");
+                        for (int j=0; j<bufList.getLength(); j++)
+                        {
+                            element = (Element) bufList.item(j);
+                            total += Integer.parseInt(element.getAttribute("cost"));
+                        }
+                        
+                        if (orderElement.getElementsByTagName("totalcost").getLength() == 0)
+                        {
+                            element = doc.createElement("totalcost");
+                            element.setTextContent(Integer.toString(total));
+                            orderElement.appendChild(element);
+                            writeXml();
+                            
+                        }
+                        else if (Integer.parseInt(orderElement.getElementsByTagName("totalcost").item(0).getTextContent()) != total) {
+                            orderElement.getElementsByTagName("totalcost").item(0).setTextContent(Integer.toString(total));
+                            writeXml();
+                        }
+                    }
                 }
             }
         }
         return total;
     }
     
-    public void removeDay(Calendar calendar) {
-        
+    public void removeDay(Calendar calendar) throws TransformerException {
+        int day, month, year;
+        Calendar thisCalendar = new GregorianCalendar();
+        NodeList dateList = doc.getElementsByTagName("date");
+        Element element;
+        for (int i=0; i<dateList.getLength(); i++)
+        {
+            element = (Element) dateList.item(i);
+            year = Integer.parseInt(element.getAttribute("year"));
+            day = Integer.parseInt(element.getAttribute("day"));
+            month = Integer.parseInt(element.getAttribute("month"));
+            thisCalendar.clear();
+            thisCalendar.set(year, month, day);
+            if (calendar.equals(thisCalendar))
+            {
+                Element el = (Element) doc.getElementsByTagName("restaurant").item(0);
+                el.removeChild(element);
+                writeXml();
+                break;
+            }
+        }   
     }
     
     public void changeOfficiantName(String oldFirstName, String oldSecondName,
-            String newFirstName, String newSecondName) {
-        
+            String newFirstName, String newSecondName) throws TransformerException {
+        Element element;
+        NodeList officiantList = doc.getElementsByTagName("officiant");
+        for (int i=0; i<officiantList.getLength(); i++) {
+            element = (Element) officiantList.item(i);
+            if (element.getAttribute("secondname").equals(oldSecondName) && 
+                    element.getAttribute("firstname").equals(oldFirstName)) {
+                element.setAttribute("secondname", newSecondName);
+                element.setAttribute("firstname", newFirstName);
+            }
+        }
+        writeXml();
     }
     
-    public void test(){
-        NodeList orderList = doc.getElementsByTagName("officiant");
-        Element element;
-        for (int i=0; i<orderList.getLength(); i++)
-        {
-            element = (Element) orderList.item(i);
-            System.out.println(element.getAttribute("secondname"));
-        }
+    private void writeXml() throws TransformerException {                       //приватный метод записи в xml
+        Transformer t = TransformerFactory.newInstance().newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File(path));
+        t.transform(source, result);
     }
 }
